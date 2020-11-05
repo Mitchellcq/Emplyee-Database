4522973dc2279cbec2de03a6adca4a02f38fb7ca
Chee Quee <mitchellcq@gmail.com>
2020-11-05 21:02:16 +1100

    wrote out all inquirer prompts, on cursory glance they all appear to wrok properly, will start to attempt abstracting out the functions

--- index.js
+++ index.js
@@ -101,22 +101,40 @@ function addEmployee() {
             type: "input",
             message: "First Name?",
             name: "first_name",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Please enter a valid name");
+                }
+                return true;
+            }
         },
         {
             type: "input",
             message: "Last Name?",
-            name: "last_name"
+            name: "last_name",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Please enter a valid name");
+                }
+                return true;
+            }
         },
         {
             type: "list",
             message: "What is the employee's role?",
             name: "role_id",
-            choices: [1, 2, 3]
+            choices: [1, 2, 3],
         },
         {
             type: "input",
             message: "Who is their manager?",
-            name: "manager_id"
+            name: "manager_id",
+            validate: function (answer) {
+                if (isNaN(answer)) {
+                    return console.log("Please enter a valid ID number");
+                }
+                return true;
+            }
         },
     ]).then(res => {
         const query = connection.query(
@@ -147,7 +165,7 @@ function removeEmployee() {
     inquirer.prompt([
         {
             type: "list",
-            message: "Please enter the full name of the employee you would like to remove",
+            message: "Please select the employee you would like to remove",
             name: "employee",
             choices: employeeList
         },
@@ -180,7 +198,8 @@ function updateEmployee() {
 
     console.log("Updating Employee info.\n");
 
-    connection.query("SELECT first_name, last_name, id FROM employees",
+    connection.query(
+        "SELECT first_name, last_name, id FROM employees",
         function (res, err) {
             if (err) throw err;
             let employees = res.map(employee => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }));
@@ -195,6 +214,12 @@ function updateEmployee() {
                     type: "input",
                     message: "please enter their new role id",
                     name: "role",
+                    validate: function (answer) {
+                        if (isNaN(answer)) {
+                            return console.log("Please enter a valid ID number");
+                        }
+                        return true;
+                    }
                 },
             ]).then(res => {
                 connection.query(`UPDATE employees SET role_id = ${res.role} WHERE id = ${res.Name}`,
@@ -211,7 +236,8 @@ function viewManagerGroups() {
 
     console.log("Viewing Manager groups.\n");
 
-    connection.query("SELECT employees.first_name, employees.last_name, roles.title AS \"role\", managers.first_name AS \"manager\" FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN employees managers ON employees.manager_id = managers.id GROUP BY employees.manager_id",
+    connection.query(
+        "SELECT employees.first_name, employees.last_name, roles.title AS \"role\", managers.first_name AS \"manager\" FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN employees managers ON employees.manager_id = managers.id GROUP BY employees.manager_id",
         function (res, err) {
             if (err) throw err;
             console.table(res);
@@ -223,7 +249,30 @@ function addDepartment() {
 
     console.log("Inserting a new Department.\n");
 
-
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "New Department name",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Please enter a valid name");
+                }
+                return true;
+            },
+        }
+    ]).then(res => {
+        const query = connection.query(
+            "INSERT INTO departments SET ?",
+            {
+                name: res.name
+            },
+            function (res, err) {
+                if (err) throw err;
+                console.log("Department added.\n")
+                init();
+            });
+    });
 
 }
 
@@ -231,28 +280,140 @@ function removeDepartment() {
 
     console.log("Removing Department.\n");
 
+    let departmentList = [];
+
+    connection.query(
+        "SELECT departments.name FROM departments", (err, res) => {
+            res.forEach(department => {
+                departments.push(department);
+            });
+        });
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "department",
+            message: "Please select the employee you would like to remove",
+            choices: departmentList,
+        },
+    ]).then(res => {
+        const query = connection.query(
+            `DELETE FROM departments WHERE name = '${res.department}'`,
+            function (res, err) {
+                if (err) throw err;
+                console.log("Department removed.\n");
+                init();
+            });
+    });
 }
 
 function viewDepartment() {
 
     console.log("Viewing all Departments.\n");
 
+    connection.query(
+        "SELECT * FROM departments",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            init();
+        });
 }
+
 
 function addRoles() {
 
     console.log("Inserting a new Role.\n");
 
+    let departments = [];
+    connection.query("SELECT * FROM departments",
+        function (err, res) {
+            if (err) throw err;
+            res.forEach(department => {
+                departments.push(department);
+            });
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "title",
+                    message: "Please enter a new Role Title.",
+                    validate: function (answer) {
+                        if (answer.length < 1) {
+                            return console.log("Please enter a valid title");
+                        }
+                        return true;
+                    }
+                },
+                {
+                    type: "input",
+                    name: "salary",
+                    message: "Please enter the Salary for the new role.",
+                    validate: function (answer) {
+                        if (isNaN(answer)) {
+                            return console.log("Please enter a valid Salary number");
+                        }
+                        return true;
+                    }
+                },
+                {
+                    type: "list",
+                    name: "department",
+                    message: "Select a department for the new role",
+                    choices: departments,
+                },
+            ]).then(res => {
+                "INSERT INTO roles SET ?",
+                {
+                    title: res.title,
+                    salary: res.salary,
+                    department_id: res.department,
+                },
+                    function (res, err) {
+                        if (err) throw err;
+                        init();
+                    }
+            });
+        });
 }
 
 function removeRoles() {
 
     console.log("Removing Role.\n");
 
+    let roleList = [];
+    connection.query(
+        "SELECT roles.title FROM roles", (err, res) => {
+            res.forEach(role => {
+                roleList.push(role);
+            });
+        });
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "Select which Role you would like to remove.",
+            choices: roleList,
+        },
+    ]).then(res => {
+        const query = connection.query(
+            `DELETE FROM roles WHERE title = '${res.role}'`,
+            function (res, err) {
+                if (err) throw err;
+                console.log("Role Removed.\n");
+                init();
+            }
+        );
+    });
 }
 
 function viewRoles() {
 
     console.log("Viewing all Roles.\n");
 
+    connection.query("SELECT roles.*, departments.name FROM roles LEFT JOIN departments ON departments.id = roles.department_id", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
 }
